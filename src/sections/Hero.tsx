0206d8ead93b46ba455c92000c1607ddb90a6ebc
javeria2108 <javeriazh@gmail.com>
2024-08-07 15:14:27 +0500

    gsap

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,27 @@
+'use client'
 import ArrowRight from '@/assets/arrow-right.svg';
 import GalaxyImage from '@/assets/galaxy.png'
 import Image from 'next/image';
 import Background from '@/assets/background.jpg'
 import NebulaImagr from '@/assets/nebula.png'
 import Waves from '@/assets/waves.svg'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 export const Hero = () => {
+  useGSAP(()=>{
+    gsap.set('.galaxy-spin', {
+      transformOrigin: 'center center', // Set the transform origin to the center of the image
+    });
+
+    gsap.to('.galaxy-spin', {
+      rotation: 360,
+      duration: 20,
+      repeat: -1,
+      ease: 'linear',
+    });
+  });
   return (
-    <section className='pt-8 pb-20 md:pt-5 md:pb-10  overflow-x-clip bg-cover bg-fixed bg-no-repeat'  style={{ backgroundImage: `url(${Background.src})` }}>
+    <section className='pt-8 pb-20 md:pt-5 md:pb-10  overflow-x-clip bg-black' >
       <div className="container">
         <div className='md:flex'>
         <div className='md:w-[478px]'>
@@ -15,7 +30,7 @@ export const Hero = () => {
           </div> */}
           <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-[#390e3f] to-[#3605a1] text-transparent bg-clip-text mt-6">Starship Scholar</h1>
           <h2 className="text-2xl md:text-3xl font-bold tracking-tighter bg-[#B8D8FF] text-transparent bg-clip-text mt-6">Innovate, Research, Achieve: Your Final Year Project Companion</h2>
-          <p className="text-xl text-[#0E0137] tracking-tight mt-6">
+          <p className="text-xl text-[#B8D8FF] tracking-tight mt-6">
           Discover, organize, and conquer your final year project with AI-powered tools and resources.
           </p>
           <div className=" flex gap-1 items-center mt-[30px]">
@@ -28,21 +43,14 @@ export const Hero = () => {
               </button>
           </div>
         </div>
-        <div className='mt-20 md:mt-0 md:h-[648px] md:flex-1 relative'>
+        <div className='mt-10 md:mt-0 md:h-[648px] md:flex-1 relative'>
   <div className="relative w-full h-full">
-  <Image 
-        src={NebulaImagr} 
-        alt='nebula image'
-        layout="fill"
-        objectFit="cover"
-        className="z-10"
-      />
       <Image 
         src={GalaxyImage} 
         alt="Starship Logo" 
         layout="fill"
         objectFit="contain"
-        className='z-20 absolute top-[-100px] md:object-none md:object-left-top'
+        className='galaxy-spin z-20 absolute top-[-50px] md:object-none md:object-left-top'
       />
   </div>
 </div>
