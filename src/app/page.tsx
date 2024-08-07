9e4d4cfc8a888bee28c83123e94a7e382e5f3b3f
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-07 21:12:52 +0400

    hero section + logo ticker done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 // import Header from "@/components/Header";
-import { Hero } from "@/sections/Hero";
-import {Header} from "@/sections/Header";
+// import { Hero } from "@/sections/Hero";
+import { Hero } from "@/sections/Hero2";
+import { Header } from "@/sections/Header";
+import { LogoTicker } from "@/sections/LogoTicker";
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <Hero/>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <LogoTicker/>
+    </>
   );
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,46 @@
+import nextLogo from "@/assets/logo-next.png";
+import tailwindLogo from "@/assets/logo-tw.png";
+import typescriptLogo from "@/assets/logo-ts.png";
+import reactLogo from "@/assets/logo-react.png";
+import mongoLogo from "@/assets/logo-mongo.png";
+import Image from "next/image";
+import nodeLogo from "@/assets/logo-node.png";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image src={reactLogo} alt="react logo" />
+            <Image
+              src={nextLogo}
+              alt="next logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={typescriptLogo}
+              alt="typescript logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={tailwindLogo}
+              alt="tailwind logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={nodeLogo}
+              alt="node logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={mongoLogo}
+              alt="mongodb logo"
+              className="logo-ticker-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
