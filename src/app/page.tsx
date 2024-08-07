709406823de029a6681e3ef6397892b404879ceb
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-07 15:11:00 +0400

    header updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import { Hero } from "@/sections/Hero";
+import {Header} from "@/sections/Header";
 export default function Home() {
   return (
     <div>
