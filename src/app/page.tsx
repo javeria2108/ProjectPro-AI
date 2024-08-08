e026a932e121c9af7b25f241f780072df25ce779
javeria2108 <javeriazh@gmail.com>
2024-08-08 22:04:57 +0500

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // import Header from "@/components/Header";
-// import { Hero } from "@/sections/Hero";
+//import { Hero } from "@/sections/Hero";
 import { Hero } from "@/sections/Hero2";
 import { Header } from "@/sections/Header";
 import { LogoTicker } from "@/sections/LogoTicker";
